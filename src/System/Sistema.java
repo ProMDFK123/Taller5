ea3ced65fc4e46109e67e1e6bf28db6427d6e6f6
Gabriel Lopez <gabriel.lopez@alumnos.ucn.cl>
2023-06-25 17:55:59 -0400

    Sistema instanced

--- src/System/Sistema.java
+++ src/System/Sistema.java
@@ -1,4 +1,13 @@
 package System;
 
+/**
+ * Interfaz del sistema con los contratos del mismo.
+ */
 public interface Sistema {
+    void iniciarSesion();
+    void buscarLibro();
+    void prestarLibro();
+    void agregarLibro();
+    void devolverLibro();
+    boolean cerrarPrograma(boolean estado);
 }
--- src/System/SistemaImpl.java
+++ src/System/SistemaImpl.java
@@ -1,4 +1,33 @@
 package System;
 
-public class SistemaImpl {
+public class SistemaImpl implements Sistema{
+    @Override
+    public void iniciarSesion() {
+
+    }
+
+    @Override
+    public void buscarLibro() {
+
+    }
+
+    @Override
+    public void prestarLibro() {
+
+    }
+
+    @Override
+    public void agregarLibro() {
+
+    }
+
+    @Override
+    public void devolverLibro() {
+
+    }
+
+    @Override
+    public boolean cerrarPrograma(boolean estado) {
+        return false;
+    }
 }
