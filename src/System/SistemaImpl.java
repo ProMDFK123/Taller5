adfa1365c592d6215ae44e4fd156f59d58032b9a
Gabriel Lopez <gabriel.lopez@alumnos.ucn.cl>
2023-06-26 14:53:23 -0400

    MainMenu Window

--- src/System/SistemaImpl.java
+++ src/System/SistemaImpl.java
@@ -36,15 +36,63 @@ public class SistemaImpl implements Sistema{
         LecturaArchivos.leerArchivoLibros(libros);
         LecturaArchivos.leerArchivoUsuarios(usuarios);
 
-        iniciarSesion(window);
-
+        //iniciarSesion(window);
     }
 
+    /**
+     * Menú principal con las opciones del programa.
+     * @param ventana a usar.
+     */
+    public void mainMenu(Ventana ventana){
+        //Definición de botones y panel.
+        JButton boton1,boton2,boton3,boton4,boton5;
+        JPanel panel;
+
+        //Visualización de la ventana.
+        ventana.setBounds(50,20,280,200);
+        ventana.setVisible(true);
+
+        //Inicialización del panel.
+        panel=new JPanel();
+        panel.setBackground(Color.pink);
+        ventana.setContentPane(panel);
+        panel.setLayout(null);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Inicialización de botones.
+        boton1=new JButton();
+        boton2=new JButton();
+        boton3=new JButton();
+        boton4=new JButton();
+        boton5=new JButton();
+
+        //Añadir botones
+        boton1.setBounds(50,10,160,25);
+        boton2.setBounds(50,40,160,25);
+        boton3.setBounds(50,70,160,25);
+        boton4.setBounds(50,100,160,25);
+        boton5.setBounds(50,130,160,25);
+        panel.add(boton1);
+        panel.add(boton2);
+        panel.add(boton3);
+        panel.add(boton4);
+        panel.add(boton5);
+        boton1.setText("Buscar Libro");
+        boton2.setText("Prestar Libro");
+        boton3.setText("Agregar Libro");
+        boton4.setText("Devolver Libro");
+        boton5.setText("Cerrar Sesión");
+    }
+
+    /**
+     * Pestaña de inicio de sesión.
+     * @param ventana a usar.
+     */
     @Override
     public void iniciarSesion(Ventana ventana) {
         //Definición de textos, botones y el panel.
         JTextField texto1,texto2;
-        JButton boton1,boton2;
+        JButton boton;
         JPanel panel;
 
         //Visibilidad de la ventana.
@@ -63,8 +111,7 @@ public class SistemaImpl implements Sistema{
         texto2=new JTextField();
 
         //Inicialización de botones
-        boton1=new JButton();
-        boton2=new JButton();
+        boton=new JButton();
 
         //Se coloca el texto y se les da un mensaje.
         texto1.setBounds(50,10,160,25);
@@ -74,16 +121,20 @@ public class SistemaImpl implements Sistema{
         panel.add(texto2);
         texto2.setText("Contraseña");
 
-        boton1.setBounds(20,110,80,25);
-        panel.add(boton1);
-        boton1.setText("logIn");
-        boton2.setBounds(180,110,80,25);
-        panel.add(boton2);
-        boton2.setText("Close");
+        boton.setBounds(50,110,80,25);
+        panel.add(boton);
+        boton.setText("logIn");
     }
 
     @Override
     public void buscarLibro(Ventana ventana) {
+        JTextField texto;
+        JButton boton;
+        JPanel panel;
+
+        ventana.setBounds(50,20,280,180);
+        ventana.setVisible(true);
+
 
     }
 
