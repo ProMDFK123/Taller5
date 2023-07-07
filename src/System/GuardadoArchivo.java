922edc61e9fa1b80e1343ba322c8d6d4203a288b
Gabriel Lopez <gabriel.lopez@alumnos.ucn.cl>
2023-07-06 21:18:53 -0400

    final version

--- src/System/GuardadoArchivo.java
+++ src/System/GuardadoArchivo.java
@@ -8,6 +8,8 @@ import Objects.Transferencia;
 import javax.swing.text.Element;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GuardadoArchivo {
     private GuardadoArchivo() {
@@ -15,48 +17,23 @@ public class GuardadoArchivo {
 
     public static void guardar(ArrayList<Elemento> lista, String fileName) {
         try{
-            FileWriter writer = new FileWriter(fileName,true);
+            Set<String> lineaUnica = new HashSet<>();
 
-            try (BufferedReader br = new BufferedReader(new FileReader("libros.txt"))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    for(Elemento elemento : lista){
-                        if(elemento instanceof Libro libro){
-                            StringBuilder sb = new StringBuilder();
-                            sb.append(libro.getIsbn()+",");
-                            sb.append(libro.getTitulo()+",");
-                            sb.append(libro.getAutor()+",");
-                            sb.append(libro.getCategoria()+",");
-                            sb.append(libro.getStock()+",");
-                            sb.append(libro.getPrecio());
-                            String bookString = sb.toString();
-
-                            if(!bookString.equals(line)){
-                                writer.write(bookString);
-                            }
-                        } else if (elemento instanceof Transferencia trans) {
-                            StringBuilder sb = new StringBuilder();
-                            sb.append(trans.getRutVendedor()+",");
-                            sb.append(trans.getNombreVendedor()+",");
-                            sb.append(trans.getApellidoVendedor()+",");
-                            sb.append(trans.getIsbnLibro()+",");
-                            sb.append(trans.getTituloLibro()+",");
-                            sb.append(trans.getTipoTransaccion());
-                            String movimiento = sb.toString();
-
-                            if(!movimiento.equals(line)){
-                                writer.write(movimiento);
-                            }
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Error al leer el archivo: " + e.getMessage());
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String linea;
+            while ((linea= reader.readLine())!=null){
+                lineaUnica.add(linea);
             }
+            reader.close();
 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            for(String unico : lineaUnica){
+                writer.write(unico);
+                writer.newLine();
+            }
             writer.close();
-        }catch (IOException e){
-            System.out.println("Ocurrio un error al actualizar el archivo: "+e.getMessage());
+        }catch (IOException ex){
+            System.out.println("Error al leer y/o escribir el archivo.");
         }
     }
 }
